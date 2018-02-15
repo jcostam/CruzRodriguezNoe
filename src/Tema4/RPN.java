d6cb0a5a5f61632d1a86d4b2cfbb8a8584cf7e56
Noé <alu20468585b@ieselcaminas.org>
2018-02-15 22:11:34 +0100

    Refactorización.  Eliminamos el metodo pushPila ya que lo unico que realiza despues de haber suprimido su variable es instanciar un new NodoPila pasandole los parametros del valor doble y arriba.

--- src/Tema4/RPN.java
+++ src/Tema4/RPN.java
@@ -1,5 +1,4 @@
 package Tema4;
-
 public	class	RPN {
 	private	String	commando;
 	private	NodoPila arriba;
@@ -8,67 +7,41 @@ public	class	RPN {
 		arriba	=	null;
 		this.commando=	commando;
 	}
-
-	public	void	pushPila(double	nuevo_dato) {
-		//NodoPila 	nuevo_nodo = {eliminamos esta variable por que solo la usaremos para pasarsela al valor de arriba
-		arriba	=	new	NodoPila(nuevo_dato,arriba);
-	}
-
 	public	double	popPila( ) {
 		double	dato_arriba	=	arriba.dato;
 		arriba	=	arriba.abajo;
 		return	dato_arriba;
 	}
-
 	public	double	resultado() {
-		double	a, b;
-		
+		double	a, b;		
 		for(int	i= 0;i<	commando.length( );	i++) {
 			//si	es	un	digito
-			if(Character.isDigit(commando.charAt(i))) {
-				//double numero;
+			if(Character.isDigit(commando.charAt(i))) {				
 				// obtener	un string a	partir	del		numero
 				String temp	="";
 				for(int j= 0; j< 100 && (Character.isDigit(commando.charAt(i)) || commando.charAt(i) =='.');j++,i++) {
 					temp = 	temp + commando.charAt(i);
 				}
 				// 	convertir	a double y	añadir a	la	pila	
-				//numero	= Double.parseDouble(temp);
-				pushPila(Double.parseDouble(temp));
-			} 
-			else if	(commando.charAt(i) ==	'+'	) {
-				b= popPila( );
-				a= popPila( );
-				pushPila(a+	b);	} 
-			else if(commando.charAt(i) =='-') {
-				b= popPila( );
-				a= popPila( );
-				pushPila(a-b);} 
-			else if(commando.charAt(i) =='*') {
-				b= popPila( );
-				a= popPila( );
-				pushPila(a*	b);
-			} 
-			else if (commando.charAt(i) =='/') {
-				b= popPila( );
-				a= popPila( );
-				pushPila(a/	b);
-			}
-			else if(commando.charAt(i) =='^') {
-				b= popPila( );
-				a= popPila( );
-				pushPila(Math.pow(a,b));}
-			else if (commando.charAt(i) =='%') {
-				b= popPila( );
-				a= popPila( );
-				pushPila(a%b);
+				arriba = new NodoPila(Double.parseDouble(temp),arriba);				
+			}else if	(commando.charAt(i) ==	'+'	) {				
+				arriba = new NodoPila(popPila( )+popPila( ),arriba);
+			}else if(commando.charAt(i) =='-') {
+				arriba = new NodoPila(popPila( )-popPila( ),arriba);				
+			}else if(commando.charAt(i) =='*') {
+				arriba = new NodoPila(popPila( )*popPila( ),arriba);
+			}else if (commando.charAt(i) =='/') {
+				arriba = new NodoPila(popPila( )/popPila( ),arriba);
+			}else if(commando.charAt(i) =='^') {			
+				arriba = new NodoPila(Math.pow(popPila( ),popPila( )),arriba);
+			}else if (commando.charAt(i) =='%') {
+				arriba = new NodoPila(popPila( )%popPila( ),arriba);			
 			} 
 			else if(commando.charAt(i) !=' ') {
 				throw	new	IllegalArgumentException( );
 			}
 		}
 		double	val	= popPila( );
-
 		if(arriba!=	null) {	
 			throw	new	IllegalArgumentException( );
 		}
--- src/Tema4/TestRPN.java
+++ src/Tema4/TestRPN.java
@@ -1,30 +1,31 @@
 package Tema4;
-
-import junit.framework.Assert;
 import junit.framework.TestCase;
-
+import junit.framework.Assert;
 public class TestRPN extends TestCase{
 	
 	private RPN suma = new RPN("5 5 + 7 +");
-	private RPN resta = new RPN("15 3 - 6 -");
+	private RPN resta = new RPN("3 15 -");
 	private RPN multi = new RPN("5 5 * 10 *");
-	private RPN divi = new RPN("250 10 / 5 /");
-	private RPN mod = new RPN("25 2 %");
-		
+	private RPN divi = new RPN("10 250 /");
+	private RPN mod = new RPN("2 25 %");
+	private RPN pow = new RPN("5 5 ^");
+ 		
 	
 	public void test() {
 		//validos para cada tipo de funcion
 		assertEquals(17.0,suma.resultado( ));
-		assertEquals(6.0,resta.resultado( ));
+		assertEquals(12.0,resta.resultado( ));
 		assertEquals(250.0, multi.resultado( ));
-		assertEquals(5.0, divi.resultado( ));
+		assertEquals(25.0, divi.resultado( ));
 		assertEquals(1.0, mod.resultado( ));
+		assertEquals(3125.0, pow.resultado( ));
 		
 		Assert.assertNotSame(suma.resultado( ), 99);
 		Assert.assertNotSame(resta.resultado( ), 99);
 		Assert.assertNotSame(multi.resultado( ), 99);
 		Assert.assertNotSame(divi.resultado( ), 99);
 		Assert.assertNotSame(mod.resultado( ), 99);
+		Assert.assertNotSame(pow.resultado( ), 99);
 	}
 		
 }
